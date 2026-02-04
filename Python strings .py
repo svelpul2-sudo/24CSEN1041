c83ece6fa56717654dde523447d47abf5658f89c
svelpul2-sudo <svelpul2@student.gitam.edu>
2026-02-04 11:36:57 +0530

    Create Python strings .py

---
+++ Python strings .py
@@ -0,0 +1,29 @@
+# Simple Python String Program
+
+name = "Python"
+
+print("Name:", name)
+print("Length of name:", len(name))
+
+print("First character:", name[0])
+print("Last character:", name[-1])
+
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+
+message = "Hello"
+result = message + " " + name
+print("Concatenation:", result)
+
+print("Reverse:", name[::-1])
+#output
+Name: Python
+Length of name: 6
+First character: P
+Last character: n
+Uppercase: PYTHON
+Lowercase: python
+Concatenation: Hello Python
+Reverse: nohtyP
+
+=== Code Execution Successful ===
